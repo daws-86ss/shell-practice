f18f79134a64e7a4683d653caf86b45e2bf9bd90
daws-86ss <arjunmsp123@gmail.com>
2025-09-29 10:38:56 +0530

    variables loading

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+
+# variables learing
+
+person1=ramesh
+person2=suresh
+
+echo "person1: hi person2"
+echo "person2: good morning person1"
+echo "ramesh: what are you learning"
+echo "person2:learning devops"
